52e3cbbd68bfbb1b2aad224336f9660fab4447ca
Vithor Freitas <vithorfreitass@gmail.com>
2023-11-04 13:21:08 -0300

    dev: envio post form login

--- users/views.py
+++ users/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
 
 
 def login(request):
-  return render(request, 'users/login.html')
+  if request.method == 'GET':
+    return render(request, 'users/login.html')
+  if request.method == 'POST':
+      email = request.POST.get('email')
+      password = request.POST.get('pass')
+      
