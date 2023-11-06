2325feabf10d8d94e306b18a81f7d19d553c5f9f
Vithor Freitas <vithorfreitass@gmail.com>
2023-11-05 22:15:37 -0300

    dev: app post

--- post/models.py
+++ post/models.py
@@ -1,7 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
+from product.models import Product
 
-# class Post(models.Model):
-#     title = models.CharField(blank=False,null=False,max_length=30,editable=True)
-#     content = models.TextField(blank=False,null=False,editable=True)
-#     rate = models.PositiveSmallIntegerField(blank=False,null=False,editable=True)
+class Post(models.Model):
+    title = models.CharField(blank=False,null=False,max_length=30,editable=True)
+    content = models.TextField(blank=False,null=False,editable=True)
+    rate = models.PositiveSmallIntegerField(blank=False,null=False,editable=False)
+    published_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    product = models.OneToOneField(Product, on_delete=models.CASCADE,related_name="about",blank=False,null=False)
+    author = models.OneToOneField(User,related_name="author",on_delete=models.CASCADE,blank=False,null=False)
+    upgrade = models.ManyToManyField(User,related_name="upgrade")
+    downgrade = models.ManyToManyField(User,related_name="downgrade")
+
 
