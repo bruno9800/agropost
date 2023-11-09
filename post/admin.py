f3b520a421d9ad123b670727ee93ce01f6071028
Vithor Freitas <vithorfreitass@gmail.com>
2023-11-09 19:13:46 -0300

    dev: admin view post

--- post/admin.py
+++ post/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title','published_at','updated_at','product','author')
+    search_fields = ['title','content','product','author']
+
+admin.site.register(Post,PostAdmin)
