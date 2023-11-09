467b35c39a41ce750a157c0846627bc4eb40e87d
Vithor Freitas <vithorfreitass@gmail.com>
2023-11-09 16:33:16 -0300

    dev: profile list admin view

--- users/admin.py
+++ users/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from .models import Profile
 
-admin.site.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('created_at','user','image')
+    search_fields = ['user']
+
+admin.site.register(Profile,ProfileAdmin)
